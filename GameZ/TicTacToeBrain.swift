d44e3f0300b695a9be6faf8a0002ccffc771b6e9
abhilash uday <abhilashuday@abhilashs-mbp.fios-router.home>
2016-11-01 22:32:22 -0400

    updated the UI and the Brain function

--- GameZ/TicTacToeBrain.swift
+++ GameZ/TicTacToeBrain.swift
@@ -9,14 +9,16 @@
 import UIKit
 
 class TicTacToeBrain {
-
     //the game board as a 2D array
     //the board is intialized to all zeroes
     //each cell of the board can be made to store the playerIdentity (i.e., 1 or 2) depending on which player occupies that cell.
     var board = [[0, 0, 0],
                  [0, 0, 0],
                  [0, 0, 0]]
-    
+    var check: Bool = true
+    var a : Int = 0
+    var b: Int = 0
+    var c : Int = 0
     
     //playerIdentity is the identity of the current player.
     //playerIdentity is intially set to 1 (for player playing "X")
@@ -27,6 +29,74 @@ class TicTacToeBrain {
     //resetGame() resets the game state
     func resetGame() {
         //code to reset the board to all zeroes and the playerIdentity to 1
+        playerIdentity = 1
+        board = [[0, 0, 0],
+                 [0, 0, 0],
+                 [0, 0, 0]]
+        check =  true
+        a=0
+        b=0
+        c=0
+    }
+    
+    func changePlayer(){
+        if(playerIdentity==1){
+            playerIdentity=2
+        }
+        else if(playerIdentity==2)
+        {
+            playerIdentity=1
+        }
+
+    }
+    
+    
+    func computeWinner()->Int {
+        if(board[0][0]==board[0][1]) && (board[0][2]==board[0][1]) && (board[0][0] != 0) {
+            a=0
+            b=1
+            c=2
+            check = false
+            let winner = board[0][1]
+            return winner
+        }
+       else if(board[1][0]==board[1][1]) && (board[1][2]==board[1][1]) && (board[1][0] != 0) {
+            check = false
+            let winner = board[1][1]
+            return winner
+        }
+        else if(board[2][0]==board[2][1]) && (board[2][2]==board[2][1]) && (board[2][0] != 0) {
+            check = false
+            let winner = board[2][1]
+            return winner
+        }
+        else if(board[0][0]==board[1][0]) && (board[2][0]==board[1][0]) && (board[0][0] != 0) {
+            check = false
+            let winner = board[1][0]
+            return winner
+        }
+        else if(board[0][1]==board[1][1]) && (board[2][1]==board[1][1]) && (board[1][1] != 0) {
+            check = false
+            let winner = board[1][1]
+            return winner
+        }
+        else if(board[0][2]==board[1][2]) && (board[2][2]==board[1][2]) && (board[1][2] != 0) {
+            check = false
+            let winner = board[1][2]
+            return winner
+        }
+        else if(board[0][0]==board[1][1]) && (board[2][2]==board[1][1]) && (board[1][1] != 0) {
+            check = false
+            let winner = board[1][1]
+            return winner
+        }
+        else if(board[0][2]==board[1][1]) && (board[2][0]==board[1][1]) && (board[1][1] != 0) {
+            check = false
+            let winner = board[1][1]
+            return winner
+        }
+
+        return 0
     }
     
     
--- GameZ/TicTacToeViewController.swift
+++ GameZ/TicTacToeViewController.swift
@@ -11,13 +11,16 @@ import UIKit
 class TicTacToeViewController: UIViewController {
     //game is an instance of TicTacToeBrain
     var game = TicTacToeBrain()
+    var playeridforwinner = 0
+    var i:Int = 0
+    var j:Int = 0
+    var count :Int = 0
     
     
     //"buttons" is an array of IBOutlets
     //The first buttion connected to "buttons" will be connected to the first array location, 
     //the second button connected to "buttons" will be connected to the secondary array location, and so on.
     @IBOutlet var buttons: [UIButton]!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,11 +29,141 @@ class TicTacToeViewController: UIViewController {
     }
     
     
-    
     //IBAction for buttons. All 9 buttons should invoke this IBAction.
     //Hint: The "tag" propery of a button can be used to find which button invoked this IBAction.
-    @IBAction func buttonPressed(button: UIButton) {
+    @IBAction func buttonPressed(button : AnyObject)
+    {
+        let index=button.tag
+        let checkValue=button.tag
         
+        if(checkValue == 0)
+        {
+            i=0
+            j=0
+        }
+        else if(checkValue == 1)
+        {
+            i=0
+            j=1
+        }
+        else if(checkValue == 2)
+        {
+            i=0
+            j=2
+        }
+        else if(checkValue == 3)
+        {
+            i=1
+            j=0
+        }
+        else if(checkValue == 4)
+        {
+            i=1
+            j=1
+        }
+        else if(checkValue == 5)
+        {
+            i=1
+            j=2
+        }
+        else if(checkValue == 6)
+        {
+            i=2
+            j=0
+        }
+        else if(checkValue == 7)
+        {
+            i=2
+            j=1
+        }
+        else
+        {
+            i=2
+            j=2
+        }
+        
+        
+        
+        
+        
+        if((game.check == true) && (game.board[i][j] == 0))
+        {
+            count = count + 1
+            if game.playerIdentity == 1{
+            button.setTitle("X", forState: UIControlState.Normal)
+            //button.setTitleColor(UIColor.blackColor(), forState:  UIControlState.Normal)
+                if(game.a == 0 && game.b == 1 && game.c == 2){
+                    button.setTitleColor(UIColor.greenColor(), forState: UIControlState.Normal)
+                }
+            }
+            else if game.playerIdentity == 2{
+            button.setTitle("0", forState: UIControlState.Normal)
+            //button.setTitleColor(UIColor.redColor(), forState:  UIControlState.Normal)
+        }
+        
+        
+        switch index{
+        case 0:
+        game.board[0][0]=game.playerIdentity
+        game.changePlayer()
+        break
+        case 1:
+            game.board[0][1]=game.playerIdentity
+             game.changePlayer()
+            break
+        case 2:
+            game.board[0][2]=game.playerIdentity
+             game.changePlayer()
+            break
+        case 3:
+            game.board[1][0]=game.playerIdentity
+             game.changePlayer()
+            break
+        case 4:
+            game.board[1][1]=game.playerIdentity
+             game.changePlayer()
+            break
+        case 5:
+            game.board[1][2]=game.playerIdentity
+             game.changePlayer()
+            break
+        case 6:
+            game.board[2][0]=game.playerIdentity
+             game.changePlayer()
+            break
+        case 7:
+            game.board[2][1]=game.playerIdentity
+             game.changePlayer()
+            break
+        case 8:
+            game.board[2][2]=game.playerIdentity
+             game.changePlayer()
+            break
+        default:
+            print("default value")
+        }
+        
+        game.computeWinner()
+        playeridforwinner=game.computeWinner()
+        if(playeridforwinner==0 && game.check == false){
+            gameOverWithWinner(nil)
+        }
+        else if((playeridforwinner == 1 || playeridforwinner == 2) && game.check == false)
+        {
+           
+            if let button = self.view.viewWithTag(0) as? UIButton
+            {
+                button.setTitleColor(UIColor.greenColor(), forState: UIControlState.Normal)
+            }
+             gameOverWithWinner(playeridforwinner)
+        }
+           
+        
+            if(count == 9){
+                gameOverWithWinner(nil)
+                
+            }
+        }
     }
     
     
@@ -66,14 +199,13 @@ class TicTacToeViewController: UIViewController {
     
     //startNewGame() resets the game state 
     func startNewGame() {
+        count = 0
         game.resetGame()
-        
+        i = 0
+        j = 0
         for i in 0...8 {
-          //  buttons[i].setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-           // buttons[i].setTitle("", forState: .Normal)
+            buttons[i].setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
+            buttons[i].setTitle("", forState: .Normal)
         }
     }
-    
-    
-    /*IMPLEMENT OTHER FUNCTIONS AS NEEDED*/
 }
